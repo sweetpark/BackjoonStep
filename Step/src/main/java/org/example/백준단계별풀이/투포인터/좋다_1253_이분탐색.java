980a181aa83a5b3de9697d4c728b9f1d07f72a73
sweetpark <wooyeong1998@gmail.com>
2025-05-15 23:59:15 +0900

    [FEAT] 백준 1253번 이분탐색/투포인터 적용

---
+++ Step/src/main/java/org/example/백준단계별풀이/투포인터/좋다_1253_이분탐색.java
@@ -0,0 +1,77 @@
+package org.example.백준단계별풀이.투포인터;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 좋다_1253_이분탐색 {
+    /***
+     * 이분탐색
+     */
+    static int[] arr;
+    static int N;
+    static int cnt;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N= Integer.parseInt(br.readLine());
+        arr = new int[N];
+        visited = new boolean[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        for(int i = 0; i < N; i++){
+            if(isGood(i)){
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+
+    }
+
+    public static boolean isGood(int index){
+        int target = arr[index];
+
+        for(int j = 0; j < N; j++){
+            if (j == index) continue;
+
+            int need = target - arr[j];
+
+            if(binarySerach(need, j, index)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean binarySerach(int key, int exclude1, int exclude2){
+        int left = 0;
+        int right = N-1;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+
+            if (mid == exclude1 || mid == exclude2) {
+                if (arr[mid] < key) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+                continue;
+            }
+
+            if(arr[mid] == key){ return true;}
+            else if(arr[mid] < key){ left = mid +1;}
+            else right = mid - 1;
+        }
+        return false;
+    }
+}
